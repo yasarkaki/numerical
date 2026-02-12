18d182f46d7155caa8affdf633c8753fec699700
Yaşar Kaki <148800866+yasarkaki@users.noreply.github.com>
2026-02-12 11:39:36 +0300

    Add files via upload

---
+++ numerical.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.sqrt(4))
+print(math.sin(math.pi/2))
+print(math.exp(math.log(10)))
+print(math.exp(3/4))
+
+print(math.factorial(5))
+print(math.inf/math.inf)
+print(complex(2,5))
+print()
